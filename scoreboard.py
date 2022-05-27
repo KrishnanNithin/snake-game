267a4ec7b9d6edeba3bc321eebf60817370c50ba
KrishnanNithin <75091510+KrishnanNithin@users.noreply.github.com>
2022-05-27 22:55:20 +0400

    save highscores

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,14 @@
 #imports
 from turtle import Turtle, Screen
 
+from sqlalchemy import Integer
+
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("highscore.txt", mode="r") as file:
+            self.high_score = int(file.read())
         self.hideturtle()
         self.goto(-30, 250)
         self.color("white")
@@ -19,5 +22,11 @@ class ScoreBoard(Turtle):
         if self.score > self.high_score:
             self.clear()
             self.high_score = self.score
+            self.write(f'Game Over! Final score = {self.score} High score = {self.high_score}', align="center",font=('Arial', 15, 'normal'))
+            self.score = 0
+            with open("highscore.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        else:
+            self.clear()
             self.write(f'Game Over! Final score = {self.score} High score = {self.high_score}', align="center",font=('Arial', 15, 'normal'))
             self.score = 0
